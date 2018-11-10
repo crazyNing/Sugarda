c95b3fa3411fa6bcad5be451d48cb2489dcfe186
crazyNing <1490514873@qq.com>
2018-11-10 22:45:28 +0800

    创建项目，导入项目所有的描述信息
    Signed-off-by: crazyNing <1490514873@qq.com>

---
+++ SugardaManagerSystem/src/main/java/com/wangNing/Sugarda/SugardaApplication.java
@@ -0,0 +1,12 @@
+package com.wangNing.Sugarda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugardaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugardaApplication.class, args);
+	}
+}
---
+++ SugardaManagerSystem/src/test/java/com/wangNing/Sugarda/SugardaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangNing.Sugarda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SugardaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

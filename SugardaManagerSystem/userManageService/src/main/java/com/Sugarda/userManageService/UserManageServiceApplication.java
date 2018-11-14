696b70aaae7f1ecda8163a82a31a350dc821a69a
crazyNing <1490514873@qq.com>
2018-11-14 16:48:34 +0800

    降低spring boot以及spring cloud版本，调通eureka注册
    Signed-off-by: crazyNing <1490514873@qq.com>

--- SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
+++ SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
@@ -2,10 +2,14 @@ package com.Sugarda.userManageService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserManageServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserManageServiceApplication.class, args);
     }

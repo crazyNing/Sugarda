4d1ebb741e8a33f7b7df72616f2328ed9d82ce0b
crazyNing <1490514873@qq.com>
2018-11-15 22:35:25 +0800

    删除无效代码
    Signed-off-by: crazyNing <1490514873@qq.com>

--- SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
+++ SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
@@ -3,19 +3,11 @@ package com.Sugarda.userManageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
-@RestController
 public class UserManageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserManageServiceApplication.class, args);
-    }
-
-    @GetMapping(value = "/test")
-    public String hello() {
-        return "hello world";
     }
 }

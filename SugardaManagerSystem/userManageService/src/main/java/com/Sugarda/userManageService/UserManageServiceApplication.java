c176caab0cd105d9e28d0811aebc30e3386d15f6
crazyNing <1490514873@qq.com>
2018-11-15 17:48:05 +0800

    新增网关服务，调通网关服务与其他服务之间的链路
    Signed-off-by: crazyNing <1490514873@qq.com>

--- SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
+++ SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
@@ -3,9 +3,6 @@ package com.Sugarda.userManageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication

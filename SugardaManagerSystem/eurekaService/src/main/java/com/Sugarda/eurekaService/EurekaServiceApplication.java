f682bf2f9b7a9cb20efc9029366c2102bb980ce2
crazyNing <1490514873@qq.com>
2018-11-19 23:30:30 +0800

    合并重复依赖到父pom文件，将文件重新整理，更改为从配置文件读取可配置信息，修改配置文件。
    Signed-off-by: crazyNing <1490514873@qq.com>

--- SugardaManagerSystem/eurekaService/src/main/java/com/Sugarda/eurekaService/EurekaServiceApplication.java
+++ SugardaManagerSystem/eurekaService/src/main/java/com/Sugarda/eurekaService/EurekaServiceApplication.java
@@ -1,5 +1,6 @@
 package com.Sugarda.eurekaService;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
 }
--- SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
+++ SugardaManagerSystem/userManageService/src/main/java/com/Sugarda/userManageService/UserManageServiceApplication.java
@@ -1,20 +1,12 @@
 package com.Sugarda.userManageService;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
-@RestController
 public class UserManageServiceApplication {
-    @GetMapping(value = "/test")
-    public String test(){
-        return "testtest";
-    }
     public static void main(String[] args) {
         SpringApplication.run(UserManageServiceApplication.class, args);
     }

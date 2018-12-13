d4ce6b6086d0a64459ed17c2da7bada58e3c2c6b
crazyNing <1490514873@qq.com>
2018-12-13 15:30:44 +0800

    修改为war包方式
    Signed-off-by: crazyNing <1490514873@qq.com>

--- SugardaManagerSystem/adminService/src/main/java/com/Sugarda/adminService/AdminServiceApplication.java
+++ SugardaManagerSystem/adminService/src/main/java/com/Sugarda/adminService/AdminServiceApplication.java
@@ -3,6 +3,8 @@ package com.Sugarda.adminService;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,25 @@ import org.springframework.context.annotation.Configuration;
 @EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
-public class AdminServiceApplication {
+/**
+ * 使用jar包运行语法 */
+//public class AdminServiceApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AdminServiceApplication.class, args);
+//	}
+//}
+/**
+ * 使用war包运行写法
+ */
+public class AdminServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(AdminServiceApplication.class);
+    }
 }
--- SugardaManagerSystem/eurekaService/src/main/java/com/Sugarda/eurekaService/EurekaServiceApplication.java
+++ SugardaManagerSystem/eurekaService/src/main/java/com/Sugarda/eurekaService/EurekaServiceApplication.java
@@ -3,13 +3,26 @@ package com.Sugarda.eurekaService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServiceApplication {
+//public class EurekaServiceApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(EurekaServiceApplication.class, args);
+//    }
+//}
+public class EurekaServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(EurekaServiceApplication.class);
     }
 }
